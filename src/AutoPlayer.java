139476a0c64c0536901cd99bab26ac6beac66183
avtseben <d00020@mail.ru>
2016-01-14 06:33:05 +0600

    Утренние тесты

--- src/AutoPlayer.java
+++ src/AutoPlayer.java
@@ -1,17 +1,28 @@
 import java.util.ArrayList;
+import java.util.*;
 
 public class AutoPlayer {
 
     private char Fig = 'X';
+    private ArrayList lineList = new ArrayList();
 
     public dot getStep()
     {
-        ArrayList lineList = new ArrayList();//List of Lines
+        ;//List of Lines
         lineList = MainClass.f1.lineBuilder(Fig);
         dot d = ((LineObj)lineList.get(0)).getDot();
-        System.out.print(lineList + "\n");
+        //System.out.print(lineList + "\n");
+        System.out.println("hi = "+ lineList.size() );
+        for(int i = 0; i < lineList.size(); i++) {
+            //((LineObj)lineList.get(i)).getLength();
+            System.out.println("hi");
+            MainClass.prt("lineLength is " + ((LineObj) lineList.get(i)).getLength());
+        }
         return d;
 
     }
+    public void showLines()
+    {
 
+    }
 }
