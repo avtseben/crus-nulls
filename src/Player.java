97d375d1062d01fb484bdc9f75aaa2316d2be9d2
Alexander Tsebenko <sas@mbh.mbhtech>
2016-01-29 06:09:29 +0600

    rutin commit

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 
-/**
- * Created by avtseben on 25.01.2016.
- */
-public class Player {
+public abstract class Player {
 
     protected char Fig;
     protected char enemyFig;
@@ -14,11 +11,9 @@ public class Player {
     public Player (char _Fig, Field _f)
     {
         Fig = _Fig;
-       // if(Fig == 'X') enemyFig = 'O';
-       // else enemyFig = 'X';
         targetField = _f;
         playerType = "default";
     }
-    public boolean doStep() { return true; }//Без этого я не могу в мэйн сделать  Player p = new HumanPlayer()
-    public String getType() { return playerType; }//Без этого я не могу в мэйн сделать  Player p = new HumanPlayer()
-}
+  abstract boolean doStep();
+  abstract String getType(); 
+} 
